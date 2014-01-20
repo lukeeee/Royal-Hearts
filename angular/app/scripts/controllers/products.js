877f8468532baa5e8ff5d7ba12ccd8ea926c5686
Silvia Nilsson <silvia.nilsson@hotmail.com>
2014-01-20 21:04:34 +0100

    anglar första commit

---
+++ angular/app/scripts/controllers/products.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularApp')
+  .controller('ProductsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
