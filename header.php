bd38746ae683a001c33c3d251e2aec86702a6c23
Lucas Viksten <lucas.viksten93@gmail.com>
2014-01-22 10:04:55 +0100

    Pagelinks in menu working

--- header.php
+++ header.php
@@ -26,25 +26,25 @@
     </head>
     <header>
 
-<div class="row">
-  <div class="col-md-6 col-md-offset-3">
-      <div id="title"><a href="index.php">MATKASSEN.SE</a></div>
-  </div>
-</div>
-
-<div class="row">
-    <div class="col-md-6 col-md-offset-3">
-       <div id="header">        
-            <div class="btn-toolbar" role="toolbar">
-                <div class="btn-group btn-group-lg btn-group-justified">
-                    <a class="btn btn-default" role="button" href="products.php">Varor</a>
-                    <a class="btn btn-default" role="button">Om</a>
-                    <a class="btn btn-default" role="button" href="login.php">Logga in</a>
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+          <div id="title"><a href="index.php">MATKASSEN.SE</a></div>
+      </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+           <div id="header">        
+                <div class="btn-toolbar" role="toolbar">
+                    <div class="btn-group btn-group-lg btn-group-justified">
+                        <a class="btn btn-default <?php if(strpos($pagetitle, "Varor") !== false) { echo "active"; } else { echo ""; } ?>" role="button" href="products.php">Varor</a>
+                        <a class="btn btn-default <?php if(strpos($pagetitle, "Om") !== false) { echo "active"; } else { echo ""; } ?>" role="button" href="about.php">Om</a>
+                        <a class="btn btn-default <?php if(strpos($pagetitle, "Logga") !== false) { echo "active"; } else { echo ""; } ?>" role="button" href="login.php">Logga in</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-</header>
-<body>
+    
+    </header>
+    <body>
