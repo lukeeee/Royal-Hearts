583a1b3a2da5da758734d89673abac0ef91533c0
Silvia Nilsson <silvia.nilsson@hotmail.com>
2014-02-04 21:12:22 +0100

    no message

--- public/administrator.php
+++ public/administrator.php
@@ -7,41 +7,74 @@ $pagetitle = "Hem | Matkassen.se";
 <?php 
 
 $admId = $_SESSION['id'];
-$_SESSION['cats'] = json_decode(file_get_contents("http://dev2-vyh.softwerk.se:8080/matkasseWS/rest/category/getall"),true);
+$cats = json_decode(file_get_contents("http://dev2-vyh.softwerk.se:8080/matkasseWS/rest/category/getall"),true);
 //$_SESSION['store'] = json_decode(file_get_contents("http://dev2-vyh.softwerk.se:8080/matkasseWS/rest/category/getall"),true);
 
  ?>
+    <div class="row">
+        <div class="col-md-5 col-md-offset-1">
+        	<div><h3>Redigera Kategorier</h3></div>
+        </div>
+    </div>
 
 <div class="row">
-      <div class="col-md-7 col-md-offset-4">
-          <div><h3>Redigera Kategorier</h3></div>
-      </div>
-</div>
-
-    <div class="col-md-3 col-md-offset-4">
-     <section>
+    <div class="col-md-4 col-md-offset-1">
+  	    <section>
         
-         <?php
-foreach($_SESSION['cats']  as $cat)
-{
+            <?php
+            foreach($cats  as $cat)
+            {
  
-echo "<div><h4 hidden>".$cat["id"]."</h4></div>";
-echo "<div><h4>".$cat["name"]."</h4></div>";
+            echo "<div><h4 hidden>".$cat["id"]."</h4></div>";
+            echo "<div><h4>".$cat["name"]."</h4></div>";
 
-echo "<a class='btn btn-default' href='/edit.php?id=".$cat['id']."&name=".urlencode($cat['name'])."'>Redigera</a>";
-   echo "<a class='btn btn-danger' href='/run.php?id=".$cat['id']."&func=adm_adm_delete'>Delete</a></br>";
-}
+            echo "<a class='btn btn-default' href='/edit.php?id=".$cat['id']."&name=".urlencode($cat['name'])."'>Redigera</a>";
+            echo "<a class='btn btn-danger' href='/run.php?id=".$cat['id']."&func=adm_adm_delete'>Delete</a></br>";
+   		    }     
  
-?>
+            ?>
 
-        <div class="row">
-      <div class="col-md-12 col-md-offset-0">
-          <div><h3><a href='/new.php'>Ny Kategori</a></h3></div>
-         
-      </div>
-</div>
+
+            <div class="row">
+         	    <div class="col-md-12 col-md-offset-0">
+             	  <div><h3><a href='/new.php'>Ny Kategori</a></h3></div>
+         		</div>
+   		    </div>
         
-     </section>
+        </section>
+
     </div>
+
+   
+        <div class="col-md-5 col-md-offset-1">
+            <div><h3>Redigera Butiker</h3></div>
+        </div>
+  
+
+    <div class="col-md-6 col-md-offset-1">
+        <section>
+        
+            <?php
+            foreach($_SESSION['cats']  as $cat)
+            {
+ 
+            echo "<div><h4 hidden>".$cat["id"]."</h4></div>";
+            echo "<div><h4>".$cat["name"]."</h4></div>";
+
+            echo "<a class='btn btn-default' href='/edit.php?id=".$cat['id']."&name=".urlencode($cat['name'])."'>Redigera</a>";
+            echo "<a class='btn btn-danger' href='/run.php?id=".$cat['id']."&func=adm_adm_delete'>Delete</a></br>";
+            }
+ 
+            ?>
+
+             <div class="row">
+                <div class="col-md-12 col-md-offset-0">
+                  <div><h3><a href='/new.php'>Ny Kategori</a></h3></div>
+                </div>
+            </div>
+        
+        </section>
+    </div>
+</div>
 
 <?php require_once(ROOT_PATH.'/footer.php'); ?>
