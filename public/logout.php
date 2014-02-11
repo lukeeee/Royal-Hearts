be789176d8c4f8cc8e312270fee36bab540b51c6
Sam Johnsson <johnsson.sam@gmail.com>
2014-02-11 15:33:48 +0100

    unset userid on logoff

--- public/logout.php
+++ public/logout.php
@@ -5,6 +5,7 @@
     unset($_SESSION['is_logged_in']);
 	unset($_SESSION['privilege']);
 	unset($_SESSION['username']);
+	unset($_SESSION['id']);
     //set_feedback("success", "You are now logged out.");
   }
 
