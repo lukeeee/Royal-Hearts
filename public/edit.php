3217d69bd84fef36ded55ae3ad1069c6fa8d929b
Silvia Nilsson <silvia.nilsson@hotmail.com>
2014-02-25 09:09:41 +0100

    no message

--- public/edit.php
+++ public/edit.php
@@ -21,12 +21,12 @@
        <h1>Redigera Kategori</h1>
       <form method="post" action="run.php?func=adm_adm_update" enctype="multipart/form-data">
         <div class="col-xs-1">
-          <input type="text" class="form-control" name="id" value="<?php echo $id?>">
+          <input type="text" class="form-control" name="id" id="cat_id" value="<?php echo $id?>">
         </div>
         <div class="col-xs-3">
           <input type="text" class="form-control" name="name" value="<?php echo $name ?>">
         </div>
-         <button type="submit" class="btn btn-default">Update</button>                             
+         <button type="submit" class="btn btn-default">Uppdatera</button>                             
       </form>
     </section>                          
   </div>                       
